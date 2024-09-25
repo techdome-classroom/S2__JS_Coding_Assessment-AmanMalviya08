37fb596acded0dc9adaff1a068ad40203d36b3ca
Divya yadav <divyayadav141203@gmail.com>
2024-09-25 14:47:33 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -3,9 +3,28 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const stack = [];
+    const mapping = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
     
+    for (let char of s) {
+        if (char in mapping) {
+            // Check for matching opening bracket
+            const topElement = stack.length === 0 ? '#' : stack.pop();
+            if (topElement !== mapping[char]) {
+                return false;
+            }
+        } else {
+            // It's an opening bracket, push onto the stack
+            stack.push(char);
+        }
+    }
+    
+    // If stack is empty, all brackets were matched
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
