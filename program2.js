01ae18f60a2d60b097ad07dab50f84aa146db6bd
Divya yadav <divyayadav141203@gmail.com>
2024-09-25 14:50:58 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,8 +3,32 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+    const length = s.length;
+
+    for (let i = 0; i < length; i++) {
+        const current = romanMap[s[i]];
+        const next = romanMap[s[i + 1]];
+
+        // If current is less than next, it indicates subtraction
+        if (next && current < next) {
+            total -= current;
+        } else {
+            total += current;
+        }
+    }
+
+    return total;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
